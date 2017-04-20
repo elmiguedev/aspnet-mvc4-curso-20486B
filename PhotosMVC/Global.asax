7946b78c7bab7bbb52e8bf90eed5a4df5d580bdc
Migue <mmoyano16@gmail.com>
2017-04-20 07:41:10 -0700

    Creacion del proyecto. Creacion del modelo basico

---
+++ PhotosMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhotosMVC.MvcApplication" Language="C#" %>
---
+++ PhotosMVC/Models/Photo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhotosMVC.Models
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+        
+        public string Title { get; set; }
+        
+        public byte[] PhotoFile { get; set; }
+        
+        public string ImageMimeType { get; set; }
+        
+        public string Descripton { get; set; }
+        
+        [DataType(DataType.DateTime)]
+        public DateTime CreateDate { get; set; }
+        
+        public string UserName { get; set; }
+        
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
